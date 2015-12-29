0ac9f703b99af22c024bbc8201f5f050ffc6f533
Stephen Foster <stephenfoster1985@gmail.com>
2015-12-29 19:21:26 +0000

    Adding Twitter Login POC

--- app/src/main/java/fitconsultants/facebookpoc/MainActivity.java
+++ app/src/main/java/fitconsultants/facebookpoc/MainActivity.java
@@ -12,20 +12,50 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    //private static final String TWITTER_KEY = "k5CLLWhCeJRQTf63HkRsZk8NM";
+    //private static final String TWITTER_SECRET = "KuhsPb0DUmLyYlOH901CD8K7eDyyKqLefCXy9ezD4R4CnbfW8T";
+
+    private static final String TWITTER_KEY = "jG6tfk6NiTZzJQS1giSk7G34l";
+    private static final String TWITTER_SECRET = "amN2fp0d8rOzulRWrtTz3Un0CbDwECfFL0wqTYNtQNaCpA3qsC";
 
     private TextView info;
     private LoginButton loginButton;
 
     private CallbackManager callbackManager;
 
+    private TwitterLoginButton twitterLoginButton;
+    private TextView status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         FacebookSdk.sdkInitialize(getApplicationContext());
 
         setContentView(R.layout.activity_main);
+
+        twitterLoginButton = (TwitterLoginButton)findViewById(R.id.twitter_login_button);
+
+        twitterLoginButton.setCallback(new LoginHandler());
+
+        System.out.println("#####" + twitterLoginButton.getCallback());
+
+        status = (TextView)findViewById(R.id.status);
+        status.setText("Status: Ready");
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -56,16 +86,46 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         });
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private class LoginHandler extends Callback<TwitterSession> {
+        @Override
+        public void success(Result<TwitterSession> result) {
+
+            System.out.println("##### success #####");
+
+            String output = "Status: " +
+                    "Your login was successful " +
+                    result.data.getUserName() +
+                    "\nAuth Token Received: " +
+                    result.data.getAuthToken().token;
+
+            System.out.println("#####" + output);
+
+            status.setText(output);
+        }
+
+        @Override
+        public void failure(TwitterException e) {
+            status.setText("Status: Login Failed");
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        //twitterLoginButton = (TwitterLoginButton)findViewById(R.id.twitter_login_button);
+
+        //twitterLoginButton.setCallback(new LoginHandler());
 
         // Logs 'install' and 'app activate' App Events.
         AppEventsLogger.activateApp(this);
